2ae548dbaa45706f66f9402bd2f873826efd841f
Philip Beck <stoneroll6@gmail.com>
2023-10-27 20:44:31 -0700

    Add approvers and contribute method

--- src/contracts/Campaign.sol
+++ src/contracts/Campaign.sol
@@ -4,9 +4,16 @@ pragma solidity ^0.8.19;
 contract Campaign {
     address public manager;
     uint public minimumContribution;
+    address[] public approvers;
 
     constructor(uint minimum) {
         manager = msg.sender;
         minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value >= minimumContribution);
+
+        approvers.push(msg.sender);
     }
 }
