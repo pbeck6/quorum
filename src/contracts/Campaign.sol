52dba437d4bc07568433678c373c4399df793256
Philip Beck <stoneroll6@gmail.com>
2023-11-01 12:04:11 -0700

    Implement createRequest func

--- src/contracts/Campaign.sol
+++ src/contracts/Campaign.sol
@@ -29,4 +29,16 @@ contract Campaign {
 
         approvers.push(msg.sender);
     }
+
+    function createRequest(string memory description, uint value, address recipient)
+        public restricted {
+            Request newRequest = Request({
+                description: description,
+                value: value,
+                recipient: recipient,
+                complete: false
+            });
+
+            requests.push(newRequest);
+    }
 }
