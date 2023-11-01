04cbbecf517c13364eda34e47f5e969fa70d18f5
Philip Beck <stoneroll6@gmail.com>
2023-11-01 11:54:38 -0700

    Add function modifier for manager-only fns

--- src/contracts/Campaign.sol
+++ src/contracts/Campaign.sol
@@ -2,9 +2,22 @@
 pragma solidity ^0.8.18;
 
 contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+
+    Request[] public requests;
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
 
     constructor(uint minimum) {
         manager = msg.sender;
