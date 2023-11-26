9381478bf064188e15c06ab72d1eed4e8d1538a6
Philip Beck <stoneroll6@gmail.com>
2023-11-26 12:28:22 -0800

    Finalize request and pay

--- src/contracts/Campaign.sol
+++ src/contracts/Campaign.sol
@@ -5,15 +5,16 @@ contract Campaign {
     struct Request {
         string description;
         uint value;
-        address recipient;
+        address payable recipient;
         bool complete;
-        int approvalCount;
+        uint approvalCount;
         mapping(address => bool) approvals;
     }
 
     Request[] public requests;
     address public manager;
     uint public minimumContribution;
+    uint public approversCount;
     mapping(address => bool) public approvers;
 
     modifier restricted() {
@@ -30,17 +31,17 @@ contract Campaign {
         require(msg.value >= minimumContribution);
 
         approvers[msg.sender] = true;
+        approversCount++;
     }
 
-    function createRequest(string memory description, uint value, address recipient)
-        public restricted {
-            Request storage newRequest = requests.push();
+    function createRequest(string memory description, uint value, address payable recipient) public restricted {
+        Request storage newRequest = requests.push();
 
-            newRequest.description = description;
-            newRequest.value = value;
-            newRequest.recipient = recipient;
-            newRequest.complete = false;
-            newRequest.approvalCount = 0;         
+        newRequest.description = description;
+        newRequest.value = value;
+        newRequest.recipient = recipient;
+        newRequest.complete = false;
+        newRequest.approvalCount = 0;         
     }
 
     function approveRequest(uint index) public {
@@ -51,5 +52,15 @@ contract Campaign {
 
         request.approvals[msg.sender] = true;
         request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+
+        require(!request.complete);
+        require(request.approvalCount > (approversCount / 2));
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
     }
 }
