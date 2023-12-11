2f5cd15f72cb81115a54e984240d97be6c549822
Philip Beck <stoneroll6@gmail.com>
2023-12-10 20:43:05 -0800

    Compile contracts with imports

---
+++ src/ethereum/compile.js
@@ -0,0 +1,42 @@
+const path = require ('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+const campaignPath = path.resolve(__dirname, 'contracts/CampaignFactory.sol');
+const source = fs.readFileSync(campaignPath, 'utf-8');
+
+function findImports(relativePath) {
+    const absolutePath = path.resolve(__dirname, 'contracts', relativePath);
+    const importSource = fs.readFileSync(absolutePath, 'utf8');
+    return { contents: importSource };
+}
+
+const input = {
+    language: 'Solidity',
+    sources: {
+        'CampaignFactory.sol' : {
+            content: source
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': [ '*' ]
+            }
+        }
+    }
+}; 
+
+const output = JSON.parse(solc.compile(JSON.stringify(input), { import: findImports }));
+
+fs.ensureDirSync(buildPath);
+
+for (let contract in output) {
+    fs.outputJSONSync(
+        path.resolve(buildPath, contract + '.json'),
+        output[contract]
+    );
+}
