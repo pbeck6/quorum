fd8501259df7ccb544069176ad916e4648dd0051
Philip Beck <stoneroll6@gmail.com>
2023-12-24 12:41:34 -0800

    Update Web3 import

--- src/ethereum/deploy.js
+++ src/ethereum/deploy.js
@@ -1,5 +1,5 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const Web3 = require('web3');
+const { Web3 } = require('web3');
 const compiledFactory = require('./build/CampaignFactory.json');
 
 const provider = new HDWalletProvider(
