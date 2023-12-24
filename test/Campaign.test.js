34c837dafe84da304d625fadf2254c238777c3df
Philip Beck <stoneroll6@gmail.com>
2023-12-24 12:05:43 -0800

    Unit test for processing Campaign request

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -88,4 +88,59 @@ describe('Campaign', () => {
         assert.equal(accounts[1], newRequest.recipient);
     });
 
+    it('processes a spend request', async () => {
+        // Contribute to campaign and become approver
+        let senderOldBalance = await web3.eth.getBalance(accounts[1]);
+        senderOldBalance = parseFloat(web3.utils.fromWei(senderOldBalance, 'ether'));
+        await campaign.methods.contribute().send({
+            from: accounts[1],
+            value: web3.utils.toWei('10', 'ether')
+        });
+
+        let senderNewBalance = await web3.eth.getBalance(accounts[1]);
+        senderNewBalance = parseFloat(web3.utils.fromWei(senderNewBalance, 'ether'));
+        assert(senderOldBalance - senderNewBalance > 10);
+
+        // Create spend request as manager
+        await campaign.methods
+            .createRequest(
+                description = 'Buy batteries',
+                value = web3.utils.toWei('5', 'ether'),
+                recipient = accounts[2]
+            ).send({
+                from: accounts[0],
+                gas: '10000000'
+            });
+
+        let newRequest = await campaign.methods.requests(0).call();
+        assert.equal(accounts[2], newRequest.recipient);
+        assert.equal(0, newRequest.approvalCount);
+        assert.equal(false, newRequest.complete);
+
+        // Approve request
+        await campaign.methods.approveRequest(0).send({
+            from: accounts[1],
+            gas: '10000000'
+        });
+
+        newRequest = await campaign.methods.requests(0).call();
+        assert.equal(1, newRequest.approvalCount);
+        assert.equal(false, newRequest.complete);
+
+        // Finalize request
+        let recipientOldBalance = await web3.eth.getBalance(accounts[2]);
+        recipientOldBalance = parseFloat(web3.utils.fromWei(recipientOldBalance, 'ether'));
+        await campaign.methods.finalizeRequest(0).send({
+            from: accounts[0],
+            gas: '10000000'
+        });
+
+        let recipientNewBalance = await web3.eth.getBalance(accounts[2]);
+        recipientNewBalance = parseFloat(web3.utils.fromWei(recipientNewBalance, 'ether'));
+        assert(recipientNewBalance - recipientOldBalance >= 5);
+
+        newRequest = await campaign.methods.requests(0).call();
+        assert.equal(true, newRequest.complete);
+    });
+
 });
