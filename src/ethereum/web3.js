6f67c5279e21afe3fc54bf01b752b6dbe7ac7a30
Philip Beck <stoneroll6@gmail.com>
2024-01-04 20:12:28 -0800

    Conditional for default provider

--- src/ethereum/web3.js
+++ src/ethereum/web3.js
@@ -1,7 +1,15 @@
 import Web3 from "web3";
  
-window.ethereum.request({ method: "eth_requestAccounts" });
+let web3;
  
-const web3 = new Web3(window.ethereum);
+if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+    window.ethereum.request({ method: "eth_requestAccounts" });
+    web3 = new Web3(window.ethereum);
+} else {
+    const provider = new Web3.providers.HttpProvider(
+        "https://sepolia.infura.io/v3/some_endpoint"
+    );
+    web3 = new Web3(provider);
+}
  
 export default web3;
