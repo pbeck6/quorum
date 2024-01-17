d9bdd9ef33d273adb48e93c7da0eb3294d774e13
Philip Beck <stoneroll6@gmail.com>
2024-01-16 18:36:13 -0800

    Add new Campaign create form

--- src/pages/campaigns/new.js
+++ src/pages/campaigns/new.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import { FormField, Button, Form, Input } from 'semantic-ui-react'
 import Layout from '../../components/Layout'; 
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
+    state = {
+        minimumContribution: ''
+    };
+
+    submitNewCampaign = async (event) => {
+        event.preventDefault();
+
+        const accounts = await web3.eth.getAccounts();
+        await factory.methods
+            .createCampaign(this.state.minimumContribution)
+            .send({
+                from: accounts[0]
+            });
+    }
+
     render() {
         return (
             <Layout>
-                <h3>New Campaign</h3>
+                <h3>Create a Campaign</h3>
+                <Form onSubmit={ this.submitNewCampaign }>
+                    <FormField>
+                        <label>Minimum contribution</label>
+                        <Input 
+                            label="wei"
+                            labelPosition="right"
+                            value={this.state.minimumContribution}
+                            onChange={event => 
+                                this.setState({ minimumContribution: event.target.value })}
+                        />
+                    </FormField>
+                    <Button primary>Create</Button>
+                </Form>
             </Layout>
         );
     }
