e986b914944590f2ed902a880e4add1f5ae3c4d6
Philip Beck <stoneroll6@gmail.com>
2024-01-23 19:53:27 -0800

    Add Campaign contract getter methods

--- src/ethereum/contracts/Campaign.sol
+++ src/ethereum/contracts/Campaign.sol
@@ -63,4 +63,20 @@ contract Campaign {
         request.recipient.transfer(request.value);
         request.complete = true;
     }
+
+    function getRequestsCount() public view returns (uint) {
+        return requests.length;
+    }
+
+    function getSummary() public view returns (
+        uint, uint, uint, uint, address
+    ){
+        return (
+            minimumContribution,
+            address(this).balance,
+            requests.length,
+            approversCount,
+            manager
+        );
+    }
 }
