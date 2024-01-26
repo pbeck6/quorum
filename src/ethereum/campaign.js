a3dd1b26ab0235f1702a96f9aa3559f7a1f4bfd8
Philip Beck <stoneroll6@gmail.com>
2024-01-25 18:50:34 -0800

    Fetch campaign details in show page

---
+++ src/ethereum/campaign.js
@@ -0,0 +1,9 @@
+import web3 from './web3';
+const compiledCampaign = require('./build/Campaign.json');
+
+export default (address) => {
+    return new web3.eth.Contract(
+        compiledCampaign.abi,
+        address
+    )
+}
--- src/pages/campaigns/[address].js
+++ src/pages/campaigns/[address].js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import { withRouter } from 'next/router';
 import Layout from '../../components/Layout';
+import getCampaign from '../../ethereum/campaign';
 
 class Campaign extends Component {
+    static async getInitialProps(context) {
+        const campaign = getCampaign(context.query.address);
+        const campaignSummary = await campaign.methods.getSummary().call();
+
+        return { campaignSummary };
+    }
+
     render() {
         return (
             <Layout>
